1d7f24b27a0c1afeb857dc41e47e672411197363
Helen Lehrer <hlehrer@outlook.com>
2022-09-13 13:22:15 -0700

    Add result divs for social media queries

--- src/index.js
+++ src/index.js
@@ -4,17 +4,42 @@ import './css/styles.css';
 
 const formSubmit = (event) => {
   event.preventDefault();
-  const tweetSearch = document.getElementById("tweet-search").value;
-  const searchResultTwt = document.getElementById("search-result-twt");
-	document.getElementById("search-result-twt").innerHTML = null;
-  const resultDisplay = document.createElement("h3");
-  searchResultTwt.append(resultDisplay);
-  resultDisplay.append(`You are searching twitter for ${tweetSearch}.`);
-  window.scrollTo({
-    top: 850,
+  const searchKeyword = document.getElementById("tweet-search").value;
+  const searchResultSocial = document.getElementById("search-result-social");
+  searchResultSocial.removeAttribute("class", "hidden");
+  //This method is Element.scrollIntoView()
+  searchResultSocial.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+
+  // Search result for twitter
+  const resultsForTwitter = document.getElementById("search-result-twitter");
+  const twitterHeading = document.createElement("h3");
+  twitterHeading.innerText = null;
+  const twitterLogo = document.createElement("img");
+  twitterLogo.setAttribute("src", "./assets/images/twitter.png");
+  resultsForTwitter.append(twitterLogo);
+  twitterHeading.append(`You are searching Twitter for ${searchKeyword}.`);
+  resultsForTwitter.append(twitterHeading);
+
+  // Search result for EventBrite
+  const resultsForEventBrite = document.getElementById("search-result-eventB");
+  resultsForEventBrite.innerText = null;
+  const evntBriteHeading = document.createElement("h3");
+  resultsForEventBrite.append(evntBriteHeading);
+  evntBriteHeading.append(`You are searching EventBrite for ${searchKeyword}.`);
+
+  // Search result for Facebook
+  const resultsForFacebook = document.getElementById("search-result-facebook");
+  resultsForFacebook.innerText = null;
+  const facebookHeading = document.createElement("h3");
+  resultsForFacebook.append(facebookHeading);
+  facebookHeading.append(`You are searching Facebook for ${searchKeyword}.`);
+
+  /*window.scrollTo({
+    top: 435,
     left: 0,
     behavior: "smooth"
   });
+  */
   document.getElementById("search-submit").reset();
 };
 
