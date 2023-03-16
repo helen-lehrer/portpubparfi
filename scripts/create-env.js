3a570015042bf5c4eb05f707b76edfcf0acd8b9b
Helen Lehrer <hlehrer@outlook.com>
2023-03-16 12:51:53 -0700

    Edit create-env script

--- scripts/create-env.js
+++ scripts/create-env.js
@@ -1,6 +1,3 @@
 const fs = require('fs')
-fs.writeFileSync('./.env', `BEARER_TOKEN_EB=${process.env.BEARER_TOKEN_EB}\n`)
-
-const fs2 = require('fs2')
-fs2.writeFileSync('./.env', `ACCESS_TOKEN_PHQ=${process.env.ACCESS_TOKEN_PHQ}\n`)
+fs.writeFileSync('./.env', `BEARER_TOKEN_EB=${process.env.BEARER_TOKEN_EB}\nACCESS_TOKEN_PHQ=${process.env.ACCESS_TOKEN_PHQ}`)
 
